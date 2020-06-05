f7ae3d6b56ec83a4bbaaef96cd517a6b2aa2064c
MElwenn <63125829+MElwenn@users.noreply.github.com>
2020-06-05 13:39:19 +0200

    Create scripts.js
    played around with math and added var favoriteFood

---
+++ js/scripts.js
@@ -0,0 +1,21 @@
+alert('Hello world');
+
+var myName = 'Bob ';
+document.write(myName);
+myName = 'John Doe ';
+document.write(myName);
+
+var simpleAddition = 2 + 2;
+document.write(simpleAddition);
+
+var size = 100;
+var doubleSize = size * 2;
+document.write(doubleSize);
+
+var size = 100; //100
+var doubleSize = size * 2;  //multiplies 'size' by 2 = 200
+var minSize = (doubleSize * 2) - (size / 2); // 200 multiplied by 2 = 400 minus half of 100 = 350
+document.write(minSize); //display 350 onsite
+
+var favoriteFood = ' Sushi is my favorite food.'; //define variable
+document.write(favoriteFood); //display variable onsite
