4865f4d611f18ab4ea8500552cd4f208051cf605
MElwenn <63125829+MElwenn@users.noreply.github.com>
2020-06-05 18:07:23 +0200

    Update scripts.js
    added array pokemonList and three pokemon objects

--- js/scripts.js
+++ js/scripts.js
@@ -1,21 +1,19 @@
-alert('Hello world');
+var pokemonList = ['Bulbasaur', 'Pikachu', 'Weedle'];
 
-var myName = 'Bob ';
-document.write(myName);
-myName = 'John Doe ';
-document.write(myName);
+var pokemon = {
+  name: 'Bulbasaur',
+  height: 7,
+  types: ['grass','poison']
+};
 
-var simpleAddition = 2 + 2;
-document.write(simpleAddition);
+var pokemon = {
+  name: 'Pikachu',
+  height: 4,
+  types: ['electric']
+};
 
-var size = 100;
-var doubleSize = size * 2;
-document.write(doubleSize);
-
-var size = 100; //100
-var doubleSize = size * 2;  //multiplies 'size' by 2 = 200
-var minSize = (doubleSize * 2) - (size / 2); // 200 multiplied by 2 = 400 minus half of 100 = 350
-document.write(minSize); //display 350 onsite
-
-var favoriteFood = ' Sushi is my favorite food.'; //define variable
-document.write(favoriteFood); //display variable onsite
+var pokemon = {
+  name: 'Weedle',
+  height: 3,
+  types: ['bug','poison']
+};
