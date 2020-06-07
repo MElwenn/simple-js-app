44bfcef3399423b8afa675d5bf2d4d966ab489ae
MElwenn <63125829+MElwenn@users.noreply.github.com>
2020-06-07 16:22:27 +0200

    Update scripts.js
    added a) array pokemonList b) object repository c) conditional to heighlight object whose height-value exceed 5

--- js/scripts.js
+++ js/scripts.js
@@ -1,23 +1,36 @@
-var pokemonList = [' Bulbasaur', ' Pikachu', ' Weedle'] // array named pokemonList
+// array pokemonList
+var repository = [
+  {
+  //  var pokemon = {
+      name: 'Bulbasaur',
+      height: 7,
+      types: ['grass','poison']
+  //  }
+  },
+  {
+    //var pokemon = {
+      name: 'Pikachu',
+      height: 4,
+      types: ['electric']
+  //  }
+  },
+  {
+    //var pokemon = {
+      name: 'Weedle',
+      height: 3,
+      types: ['bug','poison']
+  //  }
+  }
+] // End of repository
 
-var pokemonBulbasaur = {
-  name: ' Bulbasaur',
-  height: 7,
-  types: [' grass',' poison']
-};
+var pokemonList = ['Bulbasaur (height: 7) Wow, that’s big!', 'Pikachu (height: 4) ', 'Weedle (height: 3) '] //workaround for DOES NOT WORK se below
 
-var pokemonPikachu = {
-  name: ' Pikachu',
-  height: 4,
-  types: [' electric']
-};
+for (var i = 0; i < pokemonList.length; i++) {
+  if (repository.height > 5) {                 //check if the height is above the value 5
+    document.write(repository.name + repository.height + '<p>Wow, that’s big!</p>');
+  }
 
-var pokemonWeedle = {
-  name: ' Weedle',
-  height: 3,
-  types: [' bug',' poison']
-};
-
-document.write(pokemonList);
-
-document.write(pokemonWeedle.types);
+  else {
+    document.write(pokemonList[i] + '<p> </p>'); //+ '<p>height: </p>' + repository.height); DOES NOT WORK
+  }                                              //+ '<p>height: </p>' + repository.pokemon.height); DOES NOT WORK
+}
