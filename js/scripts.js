bda62e4b87a32339181ab8536e6f93f51161b50c
MElwenn <63125829+MElwenn@users.noreply.github.com>
2020-06-08 13:25:37 +0200

    Update scripts.js
    tried to fix the loop without success

--- js/scripts.js
+++ js/scripts.js
@@ -1,36 +1,39 @@
 // array pokemonList
 var repository = [
   {
-  //  var pokemon = {
+      var pokemon = {
       name: 'Bulbasaur',
       height: 7,
       types: ['grass','poison']
-  //  }
+      }
   },
   {
-    //var pokemon = {
+      var pokemon = {
       name: 'Pikachu',
       height: 4,
       types: ['electric']
-  //  }
+      }
   },
   {
-    //var pokemon = {
+      var pokemon = {
       name: 'Weedle',
       height: 3,
       types: ['bug','poison']
-  //  }
+      }
   }
 ] // End of repository
 
-var pokemonList = ['Bulbasaur (height: 7) Wow, that’s big!', 'Pikachu (height: 4) ', 'Weedle (height: 3) '] //workaround for DOES NOT WORK se below
+var pokemonList = ['Bulbasaur (height: 7)' /* Wow, that’s big!'*/, 'Pikachu (height: 4) ', 'Weedle (height: 3) '] //workaround for DOES NOT WORK se below
 
 for (var i = 0; i < pokemonList.length; i++) {
-  if (repository.height > 5) {                 //check if the height is above the value 5
-    document.write(repository.name + repository.height + '<p>Wow, that’s big!</p>');
+  if ( i == 4 ) {
+		break;
+  }
+  if (repository.pokemon.height in repository.pokemon > 5) {  //check if the height is above the value 5
+    document.write(pokemonList[i] + repository.pokemon.height + '<p>Wow, that’s big!</p>');
   }
 
   else {
-    document.write(pokemonList[i] + '<p> </p>'); //+ '<p>height: </p>' + repository.height); DOES NOT WORK
+    document.write(pokemonList[i] + repository.pokemon.height + '<p> </p>'); //+ '<p>height: </p>' + repository.height); DOES NOT WORK
   }                                              //+ '<p>height: </p>' + repository.pokemon.height); DOES NOT WORK
 }
