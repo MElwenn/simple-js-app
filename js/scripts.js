e58df312f55205614794de12c72fba1c694dff9e
MElwenn <63125829+MElwenn@users.noreply.github.com>
2020-06-11 18:06:16 +0200

    Update scripts.js
    added IIFE to avoide external code conflicts

--- js/scripts.js
+++ js/scripts.js
@@ -1,32 +1,37 @@
-// array pokemonList
-var pokemonList = [
-  {
-    name: 'Bulbasaur',
-    height: 7,
-    types: ['grass',' poison ']
-  },
-  {
-    name: 'Pikachu',
-    height: 4,
-    types: ['electric ']
-  },
-  {
-    name: 'Weedle',
-    height: 3,
-    types: ['bug',' poison ']
-  }
-];
+/* wrapping all global variables in 'Immediately Invoked Function Expression (or IIFE)' to avoid external code conflicts */
+(function () {
+  var data = {};
+  // array pokemonList
+  var pokemonList = [
+    {
+      name: 'Bulbasaur',
+      height: 7,
+      types: ['grass',' poison ']
+    },
+    {
+      name: 'Pikachu',
+      height: 4,
+      types: ['electric ']
+    },
+    {
+      name: 'Weedle',
+      height: 3,
+      types: ['bug',' poison ']
+    }
+  ];
 
-// forEach Loop
-pokemonList.forEach(function(pokemon) {
-  if (pokemon.height > 5) {              //check if the height is above the value 5 to display Wow that's big
-    document.write('<p>' + pokemon.name + ' height: ' + pokemon.height + ' Wow, that’s big!' + '</p>' + pokemon.types);
-  }
-  else {
-    document.write('<p>' + pokemon.name + ' height: ' + pokemon.height + '</p>' + pokemon.types);
-  }
-});
-
+  // forEach Loop
+  pokemonList.forEach(function(pokemon) {
+    if (pokemon.height > 5) {              //check if the height is above the value 5 to display Wow that's big
+      document.write('<p>' + pokemon.name + ' height: ' + pokemon.height + ' Wow, that’s big!' + '</p>' + pokemon.types);
+    }
+    else {
+      document.write('<p>' + pokemon.name + ' height: ' + pokemon.height + '</p>' + pokemon.types);
+    }
+  });
+})();
+/* Wrapping IIFE end */
+console.log(data); // undefined, as it's not global, but in a function!
 
 /* calculations are done here */
 function divide(dividend, divisor) { //divide function
