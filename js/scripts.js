c049eef5824090ff87d7f9f6295cd5e4d8471867
MElwenn <63125829+MElwenn@users.noreply.github.com>
2020-06-13 12:10:38 +0200

    Update scripts.js
    Re-built the IIFE wrap from scratch including all required elements getAll & add

--- js/scripts.js
+++ js/scripts.js
@@ -1,8 +1,6 @@
 /* wrapping all global variables in 'Immediately Invoked Function Expression (or IIFE)' to avoid external code conflicts */
-(function () {
-  var data = {};
-  // array pokemonList
-  var pokemonList = [
+(function () { //This is the IIFE wrap
+    var pokemonList = [ //This is the Pokemon array
     {
       name: 'Bulbasaur',
       height: 7,
@@ -18,33 +16,26 @@
       height: 3,
       types: ['bug',' poison ']
     }
-  ];
+  ];  //End of Pokemon array
+    function getAll() {   //get all items from the Pokemon array using the getAll method
+      return pokemonList;
+    }
+    function add(pokemonList) {  //add pokemonList using the push method
+      pokemonList.push(pokemonList);
+    }
 
-  // forEach Loop
-  pokemonList.forEach(function(pokemon) {
-    if (pokemon.height > 5) {              //check if the height is above the value 5 to display Wow that's big
-      document.write('<p>' + pokemon.name + ' height: ' + pokemon.height + ' Wow, that’s big!' + '</p>' + pokemon.types);
+    pokemonList.forEach(function (pokemonList) { //forEach loop to ensure only pokemons with a height > 5 are marked with ' Wow, that’s big!'
+    if (pokemonList.height > 5) {              //check if the height is above the value 5 to display Wow that's big
+      document.write('<p>' + pokemonList.name + ' height: ' + pokemonList.height + ' Wow, that’s big!' + '</p>' + pokemonList.types);
     }
     else {
-      document.write('<p>' + pokemon.name + ' height: ' + pokemon.height + '</p>' + pokemon.types);
+      document.write('<p>' + pokemonList.name + ' height: ' + pokemonList.height + '</p>' + pokemonList.types);
     }
   });
+
+    return {   //return all items from the pokemonList and all that might be added in addition
+    add: add,
+    getAll: getAll
+  };
 })();
 /* Wrapping IIFE end */
-console.log(data); // undefined, as it's not global, but in a function!
-
-/* calculations are done here */
-function divide(dividend, divisor) { //divide function
-  if (divisor === 0) {
-    return 'You are trying to divide by zero.'
-  }
-  else {
-    var divisionResult = dividend / divisor;
-    return divisionResult;
-  }
-}
-
-console.log(divide(4, 2)); // Testing the divide function
-console.log(divide(7, 0)); // Testing the divide function
-console.log(divide(1, 4)); // Testing the divide function
-console.log(divide(12, -3)); // Testing the divide function
