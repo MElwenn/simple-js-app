60fb23bac9dc96d94a214a7f115da40f5d42bf2d
MElwenn <63125829+MElwenn@users.noreply.github.com>
2020-06-15 21:06:48 +0200

    Update scripts.js
    added list & listItems to JS and HTML

--- js/scripts.js
+++ js/scripts.js
@@ -25,12 +25,33 @@
     }
 
     pokemonList.forEach(function (pokemonList) { //forEach loop to ensure only pokemons with a height > 5 are marked with ' Wow, that’s big!'
+
+    /* Task 1.6 starts here */
+        var inputs = document.querySelectorAll('input');
+        inputs.forEach(function (input) {
+          // what exactly needs to be inside this?
+        });
+        inputs[0]; // Get the first input
+        // create a list element
+        var listItem = document.createElement('li');
+        // create a button Element
+        var button = document.createElement('button');
+        var container = document.querySelector('.container');
+        container.innerHTML = '<button>Click me</button>';
+        //container.innerHTML.add(pokemonList.name);
+        button.classList.add(pokemonList.name);
+        button.innerText.add(pokemonList.name);
+        container.appendChild(button);
+    /* Task 1.6 ends here */
+
+    /* Task 1.5 code starts here
     if (pokemonList.height > 5) {              //check if the height is above the value 5 to display Wow that's big
       document.write('<p>' + pokemonList.name + ' height: ' + pokemonList.height + ' Wow, that’s big!' + '</p>' + pokemonList.types);
     }
     else {
       document.write('<p>' + pokemonList.name + ' height: ' + pokemonList.height + '</p>' + pokemonList.types);
     }
+    Task 1.5 code ends here */
   });
 
     return {   //return all items from the pokemonList and all that might be added in addition
