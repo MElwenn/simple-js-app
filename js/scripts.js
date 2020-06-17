620d2b7e74e6cb9cabf0b946843e6518f3c0b9e6
MElwenn <63125829+MElwenn@users.noreply.github.com>
2020-06-17 09:28:56 +0200

    Update scripts.js
    added function addListItem including code polishing using IIFE

--- js/scripts.js
+++ js/scripts.js
@@ -27,22 +27,26 @@
     pokemonList.forEach(function (pokemonList) { //forEach loop to ensure only pokemons with a height > 5 are marked with ' Wow, that’s big!'
 
     /* Task 1.6 starts here */
-        var hitList = document.querySelectorAll('ul');       //create a 'ul' element and assign  it to ul in HTML
-        var listItem = document.createElement('li');          //create a list element
-        var container = document.querySelector('.container'); //create a container around the button (one hitListItem within the hitList)
-        var button = document.createElement('button');        //create a button element
-        container.innerHTML = '<button>Bulbasaur</button>';   //set button innerText to be the Pokémon's name
-        //Add a class to the button using the classList.add method...
-        button.classList.add(class ='button');                         //this version works but throws a browser ERROR
-        //button.classList.add('button');                              //this version doesn't throw a browser ERROR but doesn' work
-        //document.getElementById('button').classList.add('.button');  //this version doesn't throw a browser ERROR but doesn' work
-        listItem.appendChild(button);  //append the button to the list item as its child
-        hitList.appendChild(listItem); //append the list item to the unordered list as its child
+        //function addListItem('pokemon'){  //add a function addListItem with parameter 'pokemon'
+        var pokemon = function(addListItem) {
+          var hitList = document.querySelectorAll('ul');       //create a 'ul' element and assign  it to ul in HTML
+          var listItem = document.createElement('li');          //create a list element
+          var container = document.querySelector('.container'); //create a container around the button (one hitListItem within the hitList)
+          var button = document.createElement('button');        //create a button element
+          container.innerHTML = '<button>Bulbasaur</button>';   //set button innerText to be the Pokémon's name
+          //Add a class to the button using the classList.add method...
+          button.classList.add(class ='button');                         //this version works but throws a browser ERROR
+          //button.classList.add('button');                              //this version doesn't throw a browser ERROR but doesn' work
+          //document.getElementById('button').classList.add('.button');  //this version doesn't throw a browser ERROR but doesn' work
+          listItem.appendChild(button);  //append the button to the list item as its child
+          hitList.appendChild(listItem); //append the list item to the unordered list as its child
+        };
+        function(call.addListItem('pokemon'));
     /* Task 1.6 ends here */
 
   });
 
-    return {   //return all items from the pokemonList and all that might be added in addition
+    return(addListItem){   //return all items from the pokemonList to make it available outside the IIFE
     add: add,
     getAll: getAll
   };
