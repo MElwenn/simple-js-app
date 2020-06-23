12e2068dbeadbd12cfc4b6f679ef1bc39dfae4f7
MElwenn <63125829+MElwenn@users.noreply.github.com>
2020-06-23 16:20:50 +0200

    Update scripts.js

--- js/scripts.js
+++ js/scripts.js
@@ -62,6 +62,6 @@ var pokemonRepository = (function() { //This is the IIFE wrap
 })();
 /* Wrapping IIFE end */
 
-//pokemonRepository.getAll().forEach(function(item) { //forEach loop to ensure only pokemons with a height > 5 are marked with ' Wow, that’s big!'
+// pokemonRepository.getAll().forEach(function(item) { //forEach loop to ensure only pokemons with a height > 5 are marked with ' Wow, that’s big!'
 //    pokemonRepository.addListItem(item);
 //});
