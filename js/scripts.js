606f3ede95a91a134e367074c02d49358993bf17
MElwenn <63125829+MElwenn@users.noreply.github.com>
2020-07-04 08:10:46 +0200

    Update scripts.js
    rollback

--- js/scripts.js
+++ js/scripts.js
@@ -1,4 +1,4 @@
-/* wrap ping all global variables in 'Immediately Invoked Function Expression (or IIFE)' to avoid external code conflicts */
+/* wrapping all global variables in 'Immediately Invoked Function Expression (or IIFE)' to avoid external code conflicts */
 var pokemonRepository = (function() { //This is the IIFE wrap
     var pokemonList = []; // removed pokemon objects and replaced array with an empty array
     var apiUrl = 'https://pokeapi.co/api/v2/pokemon/?limit=150'; //define apiURL
